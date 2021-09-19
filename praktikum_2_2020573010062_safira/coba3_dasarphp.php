1d6712e0401ab94d3622e7fa47736ac3bbc9d515
Safiraslw <salvirasalwa@gmail.com>
2021-09-18 17:16:35 -0700

    commit

--- praktikum_2_2020573010062_safira/coba3_dasarphp.php
+++ praktikum_2_2020573010062_safira/coba3_dasarphp.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Pemakaian Variabel 2 - safira</title>
 </head>
 <body>
-    
+    <?php
+        $jalan = "Medan - B.Aceh";
+        $noRumah = 29;
+        $blok = "4C";
+        $jumlahPenghuni = 3;
+        echo "Hasil Variabel variabel jalan + variabel noRumah adalah ";
+        echo $alamat = $jalan . $noRumah;
+        echo "<br>";
+        echo "Hasil Variabel variabel noRumah + variabel jumlahPenghuni adalah ";
+        echo $hasil = $noRumah + $jumlahPenghuni;
+        echo "<br>";
+        echo "Hasil Variabel variabel blok + variabel noRumah adalah ";
+        echo $hasil = $blok + $noRumah;
+        echo "<b>";
+    ?>
 </body>
 </html>
