e1da1aa1352159a71a0daac5223ac07ff4c00ee2
Safiraslw <salvirasalwa@gmail.com>
2021-11-02 16:27:41 +0700

    commit

--- praktikum_8_2020573010062_safira/index.php
+++ praktikum_8_2020573010062_safira/index.php
@@ -1,6 +1,5 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -11,7 +10,6 @@
     <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Sistem Informasi Peminjaman Barang Jurusan TIK</title>
 </head>
-
 <body>
     <div class="container-fluid">
         <!-- Header -->
@@ -56,5 +54,4 @@
     integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
 </body>
-
 </html>
